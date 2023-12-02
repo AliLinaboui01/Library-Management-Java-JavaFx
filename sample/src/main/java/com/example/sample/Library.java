40b64641a96e09d9c05d8557dda90356fde56a48
alilinaboui01 <alilinaboui@gmail.com>
2023-12-02 17:38:37 +0100

    new day new changes

--- sample/src/main/java/com/example/sample/Library.java
+++ sample/src/main/java/com/example/sample/Library.java
@@ -13,7 +13,11 @@ import java.sql.Statement;
 public class Library extends Application {
     @Override
     public void start(Stage stage) throws IOException, ClassNotFoundException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(Library.class.getResource("home.fxml"));
+
+//        FXMLLoader fxmlLoader = new FXMLLoader(Library.class.getResource("bookDetails.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load(), 900, 700);
        // stage.setResizable(false);
         stage.setTitle("My Library");
