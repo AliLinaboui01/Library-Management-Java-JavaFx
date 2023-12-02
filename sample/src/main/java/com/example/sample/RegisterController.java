5874a20ed457956f6dcc86f7a54693945ed69332
alilinaboui01 <alilinaboui@gmail.com>
2023-12-02 02:15:44 +0100

    new modifications

--- sample/src/main/java/com/example/sample/RegisterController.java
+++ sample/src/main/java/com/example/sample/RegisterController.java
@@ -11,6 +11,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
 public class RegisterController {
@@ -23,6 +24,8 @@ public class RegisterController {
 
     @FXML
     private TextField name;
+    @FXML
+    private Button loginLabel;
 
     @FXML
     private PasswordField password;
@@ -30,6 +33,27 @@ public class RegisterController {
     @FXML
     private Button registerButton;
     public void register(ActionEvent e){
+        final String SSl_FACTORY  ="";
 
+    }
+    public void goLogin(ActionEvent e){
+        try {
+//             Load the FXML file for the register scene
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+            Parent root = loader.load();
+
+            // Create a new scene
+            Scene nextScene = new Scene(root);
+
+            // Get the Stage from the current Node (you can adjust this if needed)
+            Stage currentStage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+
+            // Set the new scene on the stage
+            currentStage.setScene(nextScene);
+            currentStage.show();
+
+        } catch (IOException ex) {
+            ex.printStackTrace(); // Handle the exception appropriately
+        }
     }
 }
