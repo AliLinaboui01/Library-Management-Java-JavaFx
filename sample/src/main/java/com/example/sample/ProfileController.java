48f974c2fad49ff16e864fe3545a1f00879f1a1a
AKKAOUI <akkaouih17@gmail.com>
2023-12-08 22:22:04 +0100

    linked pages

--- sample/src/main/java/com/example/sample/ProfileController.java
+++ sample/src/main/java/com/example/sample/ProfileController.java
@@ -1,4 +1,103 @@
 package com.example.sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ProfileController {
+
+    public void goToHome(ActionEvent e) {
+        try {
+//             Load the FXML file for the register scene
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("home.fxml"));
+            Parent root = loader.load();
+
+            // Create a new scene
+            Scene nextScene = new Scene(root);
+
+            // Get the Stage from the current Node (you can adjust this if needed)
+            Stage currentStage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+            //currentStage.setFullScreen(true);
+
+            // Set the new scene on the stage
+            currentStage.setScene(nextScene);
+            currentStage.setFullScreen(true);
+            currentStage.show();
+
+        } catch (IOException ex) {
+            ex.printStackTrace(); // Handle the exception appropriately
+        }
+    }
+
+    public void goToAllBooks(ActionEvent event) {
+        try {
+//             Load the FXML file for the register scene
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("allBooks.fxml"));
+            Parent root = loader.load();
+
+            // Create a new scene
+            Scene nextScene = new Scene(root);
+
+            // Get the Stage from the current Node (you can adjust this if needed)
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            // Set the new scene on the stage
+            currentStage.setFullScreen(true);
+
+            currentStage.setScene(nextScene);
+            currentStage.setFullScreen(true);
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace(); // Handle the exception appropriately
+        }
+
+    }
+    public void goToMyshelf(ActionEvent event) {
+        try {
+//             Load the FXML file for the register scene
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("bookborrowd.fxml"));
+            Parent root = loader.load();
+
+            // Create a new scene
+            Scene nextScene = new Scene(root);
+
+            // Get the Stage from the current Node (you can adjust this if needed)
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            // Set the new scene on the stage
+            currentStage.setScene(nextScene);
+            currentStage.setFullScreen(true);
+
+            currentStage.show();
+
+        } catch (IOException ex) {
+            ex.printStackTrace(); // Handle the exception appropriately
+        }
+    }
+    public void gotoProfile(ActionEvent event) {
+        try {
+//             Load the FXML file for the register scene
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("profile.fxml"));
+            Parent root = loader.load();
+
+            // Create a new scene
+            Scene nextScene = new Scene(root);
+
+            // Get the Stage from the current Node (you can adjust this if needed)
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            // Set the new scene on the stage
+            currentStage.setFullScreen(true);
+
+            currentStage.setScene(nextScene);
+            currentStage.setFullScreen(true);
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace(); // Handle the exception appropriately
+        }
+    }
 }
