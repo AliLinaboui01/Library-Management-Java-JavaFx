2e88a66318aef1b753616786e4b184f09f44afac
AKKAOUI <akkaouih17@gmail.com>
2023-12-15 22:33:03 +0100

    verssion 1 user Feature

--- sample/src/main/java/BD/DataBase.java
+++ sample/src/main/java/BD/DataBase.java
@@ -3,32 +3,26 @@ import java.sql.*;
 
 public class DataBase {
     Connection conn = null;
-    String url = "jdbc:mysql://localhost:3306/library";
-    String user = "root";
-    String password = "";
+
+
     public DataBase(){}
-    public void connect() {
+    private static final String URL = "jdbc:mysql://localhost:3306/library";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
+
+
+    public void connect(){
+
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            try (Connection conn = DriverManager.getConnection(url, user, password);
-                 Statement st = conn.createStatement()) {
-
-                String query = "SELECT uname FROM students WHERE id=1";
-                ResultSet rs = st.executeQuery(query);
-
-                if (rs.next()) {
-                    String name = rs.getString("uname");
-                    System.out.println("Success connection");
-                    System.out.println("Name: " + name);
-                } else {
-                    System.out.println("No data found for id=1");
-                }
-            }
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            System.out.println("Failed connection");
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("good job success connection ");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+
     }
+
     public void closeConnection() {
         try {
             if (conn != null && !conn.isClosed()) {
